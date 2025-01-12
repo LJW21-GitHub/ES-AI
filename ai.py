fb6ffdad75413fa14a47bef2fc8d9b6b0ebab050
LJW21-GitHub <trahand.zacharie@gmail.com>
2025-01-12 18:29:22 +0100

    continuing script owo

--- ai.py
+++ ai.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-class element:
+class elementClass:
 	def __init__(self, name, masmol):
 		self.name = name
 		self.masmol = masmol
@@ -7,9 +7,10 @@ class element:
 elementDictionnary = ["helium", "oxygene"]
 variablesBeginning = ["masse"]
 variablesEnd = ["molaire"]
+variablesDictionnary = ["masse molaire"]
 
-helium = element("Hélium", 23)
-oxygene = element("Oxygène", 50)
+helium = elementClass("Hélium", 23)
+oxygene = elementClass("Oxygène", 50)
 
 def normalize(name):
 	name = name.lower()
@@ -20,6 +21,7 @@ print("Nyaah~~ uwu")
 
 while True:
 	question = input("")
+	question = normalize(question)
 	questionalnum = list([val for val in question if (val.isalnum() or val == " " or val == "." or val == ".")])
 	splitQues = question.split(" ")
 	for word in splitQues:
@@ -35,11 +37,14 @@ while True:
 			pass
 		try:
 			if word.index(variablesBeginning):
-				if splitQues([word + 1]) in variablesEnd:
+				nextWord =
+				if word+1.index(variablesEnd)
+				#if splitQues([word + 1]) in variablesEnd:
 					variablesInQues.append(" ".join(splitQues[word], splitQues[word + 1]))
 		finally:
 			pass
 
-#	if elementsInQues.len() > 0 and variablesInQues.len() > 0 and valuesInQues.len() = 0:
-#		for element in elementsInQues:
-#			return ""
+	if elementsInQues.len() > 0 and variablesInQues.len() > 0 and valuesInQues.len() = 0:
+		for element in elementsInQues:
+			for variable in variablesInQues:
+				return f"la {variable} de {getattr(element, name, )} est {getattr(element, variable, 'error')}"
