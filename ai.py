46a09deb90e56b8c833cea84aa0f9f2fd22c8d77
LJW21 <142221566+LJW21-GitHub@users.noreply.github.com>
2025-01-17 08:53:04 +0100

    warning : broken !

--- ai.py
+++ ai.py
@@ -8,16 +8,28 @@ class elementClass:
 helium = elementClass("Hélium", 23)
 oxygene = elementClass("Oxygène", 50)
 
-elementsDictionary = ["helium", "oxygene"]
-instancesDictionary = [helium, oxygene]
-variablesDictionary = ["masse molaire"]
+elementsDictionary = {
+    "helium": helium
+    "oxygene": oxygene
+}
+
+variablesDictionary = {
+    "masse molaire": "masmol"
+}
 
 
 def normalize(x):
 	x = x.lower()
 	x = x.replace("é", "e")
 	x = x.replace("è", "e")
-	return x
+	for i, c in enumerate(x):
+		if (
+			c == ","
+			and ((question[i-1]).isnumeric() if i > 0 else False)
+			and ((question[i+1]).isnumeric() if i < len(question)-1 else False)
+		):
+			question = question.replace(x, ".")
+		return x
 
 print("Nyaah~ uwu")
 
@@ -26,24 +38,17 @@ while True:
 	elementsInQues = []
 	variablesInQues = []
 	valuesInQues = []
-	question = input("")
+	question = "masse molaire hélium"
 	question = normalize(question)
-	for i, c in enumerate(question):
-		if (
-			c == ","
-			and ((question[i-1]).isnumeric() if i > 0 else False)
-			and ((question[i+1]).isnumeric() if i < len(question)-1 else False)
-		):
-			question = question.replace(x, ".")
-	
 	splitQues = question.split(" ")
 	for i, word in enumerate(splitQues):
 		if word in elementsDictionary:
 			elementsInQues.append(word)
 		if any((c in "0123456789.") for c in word):
 			valuesInQues.append(word)
-		if ("".join(list((word, splitQues[i+1] if i < len(splitQues)-1 else ""))) in variablesDictionary):
-			variablesInQues.append(variablesDictionary[i])
+		wordAndNext = " ".join(list(word, splitQues[i+1] if i < len(splitQues)-1 else ""))
+		if wordAndNext in variablesDictionary:
+			variablesInQues.append(variablesDictionary[wordAndNext])
 	print(elementsInQues)
 	print(variablesInQues)
 	print(valuesInQues)
@@ -51,9 +56,9 @@ while True:
 	print(splitQues)
 	if len(elementsInQues) > 0 and len(variablesInQues) > 0 and len(valuesInQues) == 0:
 		for variable in variablesInQues:
-			if variable == "masse molaire":
-				variable = "masmol"
+			variable = variablesDictionary[variable]
 			for element in elementsInQues:
+			    element = elementsDictionary[element]
 				print(f"la {variable} de {getattr(element, 'name', 'elementNameError')} est {getattr(element, variable, 'variableValueError')}")
 			for value in valuesInQues:
 				print(f"l'élément avec une {variable} de {value} est {''.join(x.name for x in instancesDictionary if x.variable == value)}")
