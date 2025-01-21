505e95875c4856cd3b5dca75b17fc85516fb1e2d
LJW21-GitHub <trahand.zacharie@gmail.com>
2025-01-21 22:04:50 +0100

    line 78 (for dictionary.items) broken

--- ai.py
+++ ai.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 elementsDictionary = {
-	"helium": ["Hélium", 23],
-	"oxygene": ["Oxygène", 50]
+	"helium": ["Hélium", 20.3, 3],
+	"oxygene": ["Oxygène", 50, 4],
+	"carbone": ["Carbone", 30, 5]
 }
 
 variablesDictionary = {
-	"masse molaire": ["masmol", "g/mol", 1]
+	"masse molaire": ["g/mol", 1],
+	"numero atomique": ["", 2]
 }
 
 
-def normalize(x):
+def analyze(x):
+	analysis = {
+		"elements": [],
+		"variables": [],
+		"values": [],
+		"typeOfSent": "",
+	}
 	x = x.lower()
 	x = x.replace("é", "e")
 	x = x.replace("è", "e")
@@ -21,64 +29,60 @@ def normalize(x):
 			and ((x[i+1]) in "0123456789" if i < len(question)-1 else False)
 		):
 			x = x.replace(c, ".")
-	return x
+	splitQues = x.split(" ")
+	for i, word in enumerate(splitQues):
+		if word in elementsDictionary:
+			analysis["elements"].append(word)
+		try:
+			analysis["values"].append(int(word))
+		except ValueError:
+			try:
+				analysis["values"].append(float(word))
+			except ValueError:
+				pass
+		if word in variablesDictionary:
+			analysis["variables"].append(word)
+		else:
+			wordAndNext = \
+				"".join(list(word + " " + splitQues[i+1] if i < len(splitQues)-1 else ""))
+			print(f"wordAndNext = {wordAndNext}")
+			if wordAndNext in variablesDictionary:
+				analysis["variables"].append(wordAndNext)
+	if len(analysis["variables"]) and (
+		(len(analysis["elements"]) == 0)
+		!= (len(analysis["values"]) == 0)
+	):
+		analysis["typeOfSent"] = "question"
+	return analysis
 
 
 print("Nyaah~ uwu")
 
 
 while True:
-	elementsInQues = []
-	variablesInQues = []
-	valuesInQues = []
 	question = input("")
-	question = normalize(question)
-	splitQues = question.split(" ")
-	for i, word in enumerate(splitQues):
-		if word in elementsDictionary:
-			elementsInQues.append(word)
-		try:
-			tryFloat = float(word)
-			valuesInQues.append(tryFloat)
-		except ValueError:
-			pass
-		if word in variablesDictionary:
-			variablesInQues.append(word)
-		else:
-			wordAndNext = \
-				"".join(list(word + " " + splitQues[i+1] if i < len(splitQues)-1 else ""))
-			print(f"wordAndNext = {wordAndNext}")
-			if wordAndNext in variablesDictionary:
-				variablesInQues.append(wordAndNext)
-	print(elementsInQues)
-	print(variablesInQues)
-	print(valuesInQues)
+	question = analyze(question)
 	print(question)
-	print(splitQues)
-	for variable in variablesInQues:
-		for variable in variablesInQues:
-			for element in elementsInQues:
+	if question["typeOfSent"] == "question":
+		for variable in question["variables"]:
+			for element in question["elements"]:
 				print(
 					f"la {variable} "
 					+ ("de l'" if element[0] in "aeiouyh" else "du ")
 					+ f"{elementsDictionary[element][0]} est "
-					f"{elementsDictionary[element][variablesDictionary[variable][2]]} "
-					f"{variablesDictionary[variable][1]}"
+					f"{elementsDictionary[element][variablesDictionary[variable][1]]} "
+					+ (variablesDictionary[variable][0])
 				)
-			for value in valuesInQues:
-				print(
-					f"l'élément avec une {variable} de {value} est de"
-					f"{
-						elementsDictionary[x][0]
-						if elementsDictionary[x][variablesDictionary[variable][2]] == value
-						for x in elementDictionary
-						else 'meow'
-					}"
-					f"l'élément avec une {variable} de {value} est le"
-					+ (
-						elementsDictionary[x][0]
-						for x in elementDictionary
-						if elementsDictionary[x][variablesDictionary[variable][2]] == value
-						else 'meow'
+			for value in question["values"]:
+				diff = []
+				for i, x in elementsDictionary.items():
+					diff.append(
+						abs(elementsDictionary[i][variablesDictionary[variable][1]] - value)
+						+ "/"
+						+ x
 					)
-				)
+				if 0 in diff:
+					print(
+						f"l'élément avec une {variable} de {value} est le "
+						+ diff[diff.index(0)][diff.index(0).index("/")]
+					)
