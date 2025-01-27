c2646e59f257877b7eed23b369e88499852cb65a
LJW21-GitHub <trahand.zacharie@gmail.com>
2025-01-27 19:57:58 +0100

    meowwww

--- qt6tests.py
+++ qt6tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+import PyQt6.QtGui as qtg
 import PyQt6.QtCore as qtc
 import PyQt6.QtWidgets as qtw
 
@@ -8,13 +9,47 @@ class MainWindow(qtw.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-        button = qtw.QPushButton("Press Me!")
-        button.setMinimumSize(qtc.QSize(100, 50))
-        button.setMaximumSize(qtc.QSize(300, 150))
+        widget = qtw.QWidget()
+        overallLayout = qtw.QVBoxLayout(widget)
+        scroll = qtw.QScrollArea()
+        convGridWidget = qtw.QWidget()
+        self.convGrid = qtw.QGridLayout()
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        convGridWidget.setLayout(self.convGrid)
+        convGridWidget.setSizePolicy(qtw.QSizePolicy.Policy.Ignored, qtw.QSizePolicy.Policy.Ignored)
+
+        imagelabel = qtw.QLabel()
+        textLabel = qtw.QLabel("test")
+        image = qtg.QPixmap("avatar.png")
+        image = image.scaled(qtc.QSize(75, 75))
+        imagelabel.setPixmap(image)
+
+        self.convGrid.addWidget(
+            imagelabel, 0, 0, qtc.Qt.AlignmentFlag.AlignCenter)
+        self.convGrid.addWidget(
+            textLabel, 0, 1, qtc.Qt.AlignmentFlag.AlignVCenter)
+        self.convGrid.setColumnMinimumWidth(0, 125)
+        self.convGrid.setColumnStretch(1, 1)
+        scroll.setWidget(convGridWidget)
+        scroll.setSizePolicy(qtw.QSizePolicy.Policy.Ignored, qtw.QSizePolicy.Policy.Ignored)
+
+        self.input = qtw.QLineEdit()
+        self.input.setMaximumHeight(100)
+
+        overallLayout.addWidget(scroll)
+        overallLayout.addWidget(self.input)
+
+        self.setCentralWidget(widget)
+
+        self.input.returnPressed.connect(self.add_a_row)
+
+    def add_a_row(self):
+        self.convGrid.addWidget(
+            qtw.QLabel(self.input.text()),
+            self.convGrid.rowCount() + 1,
+            1,
+            qtc.Qt.AlignmentFlag.AlignVCenter
+        )
 
 
 app = qtw.QApplication(sys.argv)
