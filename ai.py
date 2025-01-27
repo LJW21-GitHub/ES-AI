f2d55217c129c4d9bfbbb67bd9a61ad798b5e904
LJW21-GitHub <trahand.zacharie@gmail.com>
2025-01-27 19:57:36 +0100

    meow

--- ai.py
+++ ai.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
-elementsDictionary = {
+#!/bin/python3.12
+elemDict = {
 	"helium": ["Hélium", 20.3, 3],
 	"oxygene": ["Oxygène", 50, 4],
 	"carbone": ["Carbone", 30, 5]
 }
 
-variablesDictionary = {
+varDict = {
 	"masse molaire": ["masse molaire", "g/mol", 1, "f"],
 	"numero atomique": ["numéro atomique", "", 2, "m"]
 }
@@ -25,13 +25,13 @@ def analyze(x):
 	for i, c in enumerate(x):
 		if (
 			c == ","
-			and ((x[i-1]) in "0123456789" if i > 0 else False)
-			and ((x[i+1]) in "0123456789" if i < len(question)-1 else False)
+			and ((x[i - 1]) in "0123456789" if i > 0 else False)
+			and ((x[i + 1]) in "0123456789" if i < len(question) - 1 else False)
 		):
 			x = x.replace(c, ".")
 	splitQues = x.split(" ")
 	for i, word in enumerate(splitQues):
-		if word in elementsDictionary:
+		if word in elemDict:
 			analysis["elements"].append(word)
 		try:
 			analysis["values"].append(int(word))
@@ -40,12 +40,15 @@ def analyze(x):
 				analysis["values"].append(float(word))
 			except ValueError:
 				pass
-		if word in variablesDictionary:
+		if word in varDict:
 			analysis["variables"].append(word)
 		else:
 			wordAndNext = \
-				"".join(list(word + " " + splitQues[i+1] if i < len(splitQues)-1 else ""))
-			if wordAndNext in variablesDictionary:
+				"".join(list(
+					word + " " + splitQues[i + 1]
+					if i < len(splitQues) - 1 else "")
+				)
+			if wordAndNext in varDict:
 				analysis["variables"].append(wordAndNext)
 	if len(analysis["variables"]) and (
 		(len(analysis["elements"]) == 0)
@@ -66,29 +69,30 @@ while True:
 		for variable in question["variables"]:
 			for element in question["elements"]:
 				print(
-					("la " if variablesDictionary[variable][3] == "f" else "le ")
-					+ f"{variablesDictionary[variable][0]} "
+					("la " if varDict[variable][3] == "f" else "le ")
+					+ f"{varDict[variable][0]} "
 					+ ("de l'" if element[0] in "aeiouyh" else "du ")
-					+ f"{elementsDictionary[element][0]} est "
-					f"{elementsDictionary[element][variablesDictionary[variable][2]]} "
-					+ (variablesDictionary[variable][1])
+					+ f"{elemDict[element][0]} est "
+					f"{elemDict[element][varDict[variable][2]]} "
+					+ (varDict[variable][1])
 				)
 			for value in question["values"]:
+				print(value)
 				diff = {}
-				for i, x in elementsDictionary.items():
+				for i, x in elemDict.items():
 					diff[str(i)] = abs(
-						x[variablesDictionary[variable][2]]
+						x[varDict[variable][2]]
 						- value
 					)
 				print(diff)
 				if 0 in diff.values():
+					ans = "".join(list(
+						i for i, x in diff.items() if min(diff.values()) == x))
 					print(
 						"l'élément avec une "
 						+ variable
 						+ " de "
-						+ value
-						+ " est le "
-						+ (
-							"".join(list(i)) for i, x in diff.items() if min(diff.values()) == x
-						)
+						+ str(value)
+						+ (" est l'" if ans[0] in "aeiouyh" else " est le ")
+						+ elemDict[ans][0]
 					)
