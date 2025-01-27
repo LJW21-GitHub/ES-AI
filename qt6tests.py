9e42951646b5d74f53f50e9d9302d38bf47d8ab3
LJW21-GitHub <trahand.zacharie@gmail.com>
2025-01-27 23:09:44 +0100

    WORKINNNNNNG

--- qt6tests.py
+++ qt6tests.py
@@ -9,47 +9,59 @@ class MainWindow(qtw.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        widget = qtw.QWidget()
-        overallLayout = qtw.QVBoxLayout(widget)
+        self.widget = qtw.QWidget()
+        overallLayout = qtw.QVBoxLayout(self.widget)
         scroll = qtw.QScrollArea()
         convGridWidget = qtw.QWidget()
-        self.convGrid = qtw.QGridLayout()
+        self.convGrid = qtw.QGridLayout(convGridWidget)
 
-        convGridWidget.setLayout(self.convGrid)
-        convGridWidget.setSizePolicy(qtw.QSizePolicy.Policy.Ignored, qtw.QSizePolicy.Policy.Ignored)
+        convGridWidget.setSizePolicy(
+            qtw.QSizePolicy.Policy.Expanding,
+            qtw.QSizePolicy.Policy.Expanding)
 
-        imagelabel = qtw.QLabel()
+        imageLabel = qtw.QLabel()
         textLabel = qtw.QLabel("test")
         image = qtg.QPixmap("avatar.png")
-        image = image.scaled(qtc.QSize(75, 75))
-        imagelabel.setPixmap(image)
+        image = image.scaled(qtc.QSize(25, 25))
+        imageLabel.setPixmap(image)
+        textLabel.setMaximumHeight(75)
 
         self.convGrid.addWidget(
-            imagelabel, 0, 0, qtc.Qt.AlignmentFlag.AlignCenter)
+            imageLabel, 0, 0, qtc.Qt.AlignmentFlag.AlignCenter)
         self.convGrid.addWidget(
             textLabel, 0, 1, qtc.Qt.AlignmentFlag.AlignVCenter)
-        self.convGrid.setColumnMinimumWidth(0, 125)
+        self.convGrid.setColumnMinimumWidth(0, 50)
+
         self.convGrid.setColumnStretch(1, 1)
+        self.convGrid.setRowStretch(0, 0)
         scroll.setWidget(convGridWidget)
-        scroll.setSizePolicy(qtw.QSizePolicy.Policy.Ignored, qtw.QSizePolicy.Policy.Ignored)
+        scroll.setWidgetResizable(True)
+        scroll.setSizePolicy(
+            qtw.QSizePolicy.Policy.Expanding, qtw.QSizePolicy.Policy.Expanding)
 
         self.input = qtw.QLineEdit()
-        self.input.setMaximumHeight(100)
+        self.input.setMaximumHeight(50)
+
+        self.convGrid.setRowStretch(self.convGrid.rowCount(), 1)
 
         overallLayout.addWidget(scroll)
         overallLayout.addWidget(self.input)
 
-        self.setCentralWidget(widget)
+        self.setCentralWidget(self.widget)
 
         self.input.returnPressed.connect(self.add_a_row)
 
     def add_a_row(self):
+        self.convGrid.setRowStretch(self.convGrid.rowCount() - 1, 0)
         self.convGrid.addWidget(
             qtw.QLabel(self.input.text()),
-            self.convGrid.rowCount() + 1,
+            self.convGrid.rowCount() - 1,
             1,
             qtc.Qt.AlignmentFlag.AlignVCenter
         )
+        self.convGrid.setRowMinimumHeight(self.convGrid.rowCount() - 1, 25)
+        self.convGrid.setRowStretch(self.convGrid.rowCount(), 1)
+        self.input.clear()
 
 
 app = qtw.QApplication(sys.argv)
