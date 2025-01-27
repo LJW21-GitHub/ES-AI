5f77da6ba43ee17a71d3937552ef8c628bb35ceb
LJW21-GitHub <trahand.zacharie@gmail.com>
2025-01-27 23:12:08 +0100

    FUCKING WORKING

--- ai.py
+++ ai.py
@@ -1,4 +1,104 @@
-#!/bin/python3.12
+#!/usr/bin/python3
+import sys
+import PyQt6.QtGui as qtg
+import PyQt6.QtCore as qtc
+import PyQt6.QtWidgets as qtw
+
+
+class MainWindow(qtw.QMainWindow):
+	def __init__(self):
+		super().__init__()
+
+		self.widget = qtw.QWidget()
+		overallLayout = qtw.QVBoxLayout(self.widget)
+		scroll = qtw.QScrollArea()
+		convGridWidget = qtw.QWidget()
+		self.convGrid = qtw.QGridLayout(convGridWidget)
+
+		convGridWidget.setSizePolicy(
+			qtw.QSizePolicy.Policy.Expanding,
+			qtw.QSizePolicy.Policy.Expanding)
+
+		imageLabel = qtw.QLabel()
+		textLabel = qtw.QLabel(
+			"Bonjour ! Comment puis-je vous aidez sur le tableau périodique ?")
+		textLabel.setStyleSheet("background: #32373c")
+		self.avatar = qtg.QPixmap("avatar.png")
+		self.avatar = self.avatar.scaled(qtc.QSize(25, 25))
+		self.ai = qtg.QPixmap("ai.png")
+		self.ai = self.ai.scaled(qtc.QSize(25, 25))
+		imageLabel.setPixmap(self.ai)
+		textLabel.setMinimumHeight(35)
+		textLabel.setMargin(10)
+
+		self.convGrid.addWidget(
+			imageLabel, 0, 0, qtc.Qt.AlignmentFlag.AlignCenter)
+		self.convGrid.addWidget(
+			textLabel, 0, 1, qtc.Qt.AlignmentFlag.AlignVCenter)
+		self.convGrid.setColumnMinimumWidth(0, 0)
+		self.convGrid.setVerticalSpacing(0)
+
+		self.convGrid.setColumnStretch(1, 1)
+		self.convGrid.setRowStretch(0, 0)
+		scroll.setWidget(convGridWidget)
+		scroll.setWidgetResizable(True)
+		scroll.setSizePolicy(
+			qtw.QSizePolicy.Policy.Expanding, qtw.QSizePolicy.Policy.Expanding)
+
+		self.input = qtw.QLineEdit()
+		self.input.setMaximumHeight(50)
+
+		self.convGrid.setRowStretch(self.convGrid.rowCount(), 1)
+
+		overallLayout.addWidget(scroll)
+		overallLayout.addWidget(self.input)
+
+		self.setCentralWidget(self.widget)
+
+		self.input.returnPressed.connect(self.add_a_row)
+
+	def add_a_row(self):
+		self.convGrid.setRowStretch(self.convGrid.rowCount() - 1, 0)
+		inputLabel = qtw.QLabel(self.input.text())
+		inputLabel.setMinimumHeight(35)
+		inputLabel.setMargin(10)
+		avatarLabel = qtw.QLabel()
+		avatarLabel.setPixmap(self.avatar)
+		self.convGrid.addWidget(
+			inputLabel,
+			self.convGrid.rowCount() - 1,
+			1,
+			qtc.Qt.AlignmentFlag.AlignVCenter
+		)
+		self.convGrid.addWidget(
+			avatarLabel,
+			self.convGrid.rowCount() - 1,
+			0,
+			qtc.Qt.AlignmentFlag.AlignCenter
+		)
+		answer = treatment(self.input.text())
+		answerLabel = qtw.QLabel(answer)
+		answerLabel.setStyleSheet("background: #32373c")
+		answerLabel.setMinimumHeight(35)
+		answerLabel.setMargin(10)
+		aiLabel = qtw.QLabel()
+		aiLabel.setPixmap(self.ai)
+		self.convGrid.addWidget(
+			answerLabel,
+			self.convGrid.rowCount(),
+			1,
+			qtc.Qt.AlignmentFlag.AlignVCenter
+		)
+		self.convGrid.addWidget(
+			aiLabel,
+			self.convGrid.rowCount() - 1,
+			0,
+			qtc.Qt.AlignmentFlag.AlignCenter
+		)
+		self.convGrid.setRowStretch(self.convGrid.rowCount() + 1, 1)
+		self.input.clear()
+
+
 elemDict = {
 	"helium": ["Hélium", 20.3, 3],
 	"oxygene": ["Oxygène", 50, 4],
@@ -26,7 +126,7 @@ def analyze(x):
 		if (
 			c == ","
 			and ((x[i - 1]) in "0123456789" if i > 0 else False)
-			and ((x[i + 1]) in "0123456789" if i < len(question) - 1 else False)
+			and ((x[i + 1]) in "0123456789" if i < len(x) - 1 else False)
 		):
 			x = x.replace(c, ".")
 	splitQues = x.split(" ")
@@ -61,14 +161,15 @@ def analyze(x):
 print("Nyaah~ uwu")
 
 
-while True:
-	question = input("")
+def treatment(question):
+	# question = input("")
+	print(question)
 	question = analyze(question)
 	print(question)
 	if question["typeOfSent"] == "question":
 		for variable in question["variables"]:
 			for element in question["elements"]:
-				print(
+				return (
 					("la " if varDict[variable][3] == "f" else "le ")
 					+ f"{varDict[variable][0]} "
 					+ ("de l'" if element[0] in "aeiouyh" else "du ")
@@ -77,7 +178,6 @@ while True:
 					+ (varDict[variable][1])
 				)
 			for value in question["values"]:
-				print(value)
 				diff = {}
 				for i, x in elemDict.items():
 					diff[str(i)] = abs(
@@ -88,7 +188,7 @@ while True:
 				if 0 in diff.values():
 					ans = "".join(list(
 						i for i, x in diff.items() if min(diff.values()) == x))
-					print(
+					return (
 						"l'élément avec une "
 						+ variable
 						+ " de "
@@ -96,3 +196,12 @@ while True:
 						+ (" est l'" if ans[0] in "aeiouyh" else " est le ")
 						+ elemDict[ans][0]
 					)
+
+
+app = qtw.QApplication(sys.argv)
+
+window = MainWindow()
+
+window.show()
+
+app.exec()
