6a51f91e7295ec4c6dc03e6d3e12b9236641298f
LJW21 <142221566+LJW21-GitHub@users.noreply.github.com>
2025-01-31 08:16:47 +0100

    Update ai_cli.py

--- ai_cli.py
+++ ai_cli.py
@@ -1,8 +1,41 @@
 elemDict = {
-	"helium": ["Hélium", 20.3, 3],
-	"oxygene": ["Oxygène", 50, 4],
-	"carbone": ["Carbone", 30, 5]
+	"hydrogene": ["Hydrogène", 1.008, 1],
+	"helium": ["Hélium", 4.003, 2],
+	"lithium": ["Lithhium", 6.94, 3],
+	"beryllium": ["Béryllium", 9.012, 4],
+	"bore": ["Bore", 10.81, 5],
+	"carbone": ["Carbone", 12.01, 6],
+	"azote": ["Azote", 14.01, 7],
+	"oxygene": ["Oxygène", 16, 8],
+	"fluor": ["Fluor", 19, 9],
+	"neon": ["Néon", 20.18, 10],
+	"sodium": ["Sodium", 22.99, 11],
+	"magnesium": ["Magnésium", 24.31, 12],
+	"aluminium": ["Aluminium", 26.98, 13],
+	"silicium": ["Silicium", 28.09, 14],
+	"phosphore": ["Phosphore", 30.97, 15],
+	"soufre": ["Soufre", 32.06, 16],
+	"chlore": ["Chlore", 35.45, 17],
+	"argon": ["Argon", 39.95, 18],
+	"potassium": ["Potassium", 39.1, 19],
+	"calcium": ["Calcium", 40.08, 20],
+	"scandium": ["Scandium", 44.96, 21],
+	"titane": ["Titane", 47.87, 22],
+	"vanadium": ["Vanadium", 50.94, 23],
+	"chrome": ["Chrome", 52, 24],
+	"manganese": ["Manganèse", 54.94, 25],
+	"fer": ["Fer", 55.85, 26],
+	"cobalt": ["Cobalt", 58.93, 27],
+	"nickel": ["Nickel", 58.69, 28],
+	"cuivre": ["Cuivre", 63.55, 29],
+	"zinc": ["Zinc", 65.38, 30],
+	"gallium": ["Gallium", 69.72, 31],
+	"germanium": ["Germanium", 72.63, 32],
+	"arsenic": ["Arsenic", 74.92, 33],
+	"selenium": ["Sélénium", 78.97, 34],
+	"brome": ["Brome", 79.9, 35]
 }
+
 
 varDict = {
 	"masse molaire": ["masse molaire", "g/mol", 1, "f"],
@@ -95,4 +128,4 @@ def treatment():
 
 print("Nyaah~ uwu")
 while True:
-    print(treatment())
+	print(treatment())
